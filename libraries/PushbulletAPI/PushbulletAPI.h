b0227fee87d8b49cddeb3aabd46c606b30986420
bryankettle <42826207+bryankettle@users.noreply.github.com>
2018-10-03 08:50:33 +0800

    Update 1.01
    removed the annoying "PB" in commands
    added Pushbullet's sms function (not tested because it only works on US)

--- libraries/PushbulletAPI/PushbulletAPI.h
+++ libraries/PushbulletAPI/PushbulletAPI.h
@@ -18,10 +18,11 @@ using namespace std;
 class PushbulletAPI {
 	public:
 		PushbulletAPI(String accesst); //initialize Pushbullet. requires an access token
-		void PBnotify(String title, String body); // sends a push notification
-		void PBnotify(String title, String body, String fileurl); //sends a push notification with an image, currently only jpeg is supported. the fileurl is the http address of the image to be sent  
+		void notify(String title, String body); // sends a push notification
+		void notify(String title, String body, String fileurl); //sends a push notification with an image, currently only jpeg is supported. the fileurl is the http address of the image to be sent  
 		void PBconnect(); //connects to Pushbullet API
-		String PBupload(String filename); // uploads a file to PushBullet. the output will be the file url
+		String upload(String filename); // uploads a file to PushBullet. the output will be the file url
+		void sms(String message, String phoneNumber); // sends a SMS. only available in the US
 	private:
 		String accesstoken; //access token is stored here
 		WiFiClientSecure client; //PB client
